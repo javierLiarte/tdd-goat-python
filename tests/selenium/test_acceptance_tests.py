34d9ddaa84bd439e90ad6f5b49831fc5ac1dc43e
Javier Liarte <jliarte@gmail.com>
2014-09-26 10:04:40 +0200

    Functional test now checks we can input a to-do item

--- tests/selenium/test_acceptance_tests.py
+++ tests/selenium/test_acceptance_tests.py
@@ -1,4 +1,5 @@
 import pytest
+from selenium.webdriver.common.keys import Keys
 
 def test_can_start_a_list_and_retrieve_it_later(b):
 		# Edith has heard about a cool new online to-do app. She goes
@@ -7,18 +8,28 @@ def test_can_start_a_list_and_retrieve_it_later(b):
 
 		# She notices the page title and the header mention to-do lists
 		assert 'To-Do' in b.title, 'Browser title was ' + b.title
-		raise AssertionError('Finish the test!')
+		header_text = b.find_element_by_tag_name('h1').text
+		assert 'To-Do' in header_text
 
 		# She is invited to enter a to-do item straight away
+		inputbox = b.find_element_by_id('id_new_item')
+		assert inputbox.get_attribute('placeholder') == 'Enter a to-do item'
 
 		# She types "Buy peacock feathers" into a text box (Edith's hobby
 		# is tying fly-fishing lures)
+		inputbox.send_keys('Buy peacock feathers')
 
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
+		inputbox.send_keys(Keys.ENTER)
+
+		table = b.find_element_by_id('id_list_table')
+		rows = find.elements_by_tag_name('tr')
+		assert any(row.text == '1. Buy peacock feathers' for row in rows)
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+		raise AssertionError('Finish the test!')
 
 		# The page updates again, and now shows both items on her list
 
