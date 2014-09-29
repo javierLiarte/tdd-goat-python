b905848bd95c30361ae457cb47e75b840a3be8ab
Javier Liarte <jliarte@gmail.com>
2014-09-29 19:55:21 +0200

    Created a new URL for creating a new list via POST

--- lists/tests/test_lists.py
+++ lists/tests/test_lists.py
@@ -6,6 +6,7 @@ from django.template.loader import render_to_string
 from lists.views import home_page
 from lists.models import Item
 
+testserver_url = 'http://testserver'
 
 def test_root_url_resolves_to_home_page_view():
   found = resolve('/')
@@ -18,35 +19,6 @@ def test_home_page_returns_correct_html():
   expected_html = render_to_string('home.html')
   assert response.content.decode() == expected_html
 
-
-@pytest.mark.django_db
-def test_home_page_can_save_a_POST_request():
-  request = HttpRequest()
-  request.method = 'POST'
-  request.POST['item_text'] = 'A new list item'
-
-  response = home_page(request)
-
-  assert Item.objects.count() == 1
-  new_item = Item.objects.first()
-  assert new_item.text == 'A new list item'
-
-@pytest.mark.django_db
-def test_home_page_redirects_after_POST():
-  request = HttpRequest()
-  request.method = 'POST'
-  request.POST['item_text'] = 'A new list item'
-
-  response = home_page(request)
-
-  assert response.status_code == 302
-  assert response['location'] == '/lists/the-only-list-in-the-world/'
-
-@pytest.mark.django_db
-def test_home_page_only_saves_items_when_necessary():
-  request = HttpRequest()
-  home_page(request)
-  assert Item.objects.count() == 0
 
 class TestListView(object):
 
@@ -65,3 +37,23 @@ class TestListView(object):
 
     assert 'itemey 1' in response.content.decode()
     assert 'itemey 2' in response.content.decode()
+
+class TestNewList(object):
+
+  @pytest.mark.django_db
+  def test_saving_a_POST_request(self, client):
+    client.post('/lists/add',
+      data={'item_text': 'A new list item'}
+    )
+    assert Item.objects.count() == 1
+    new_item = Item.objects.first()
+    assert new_item.text == 'A new list item'
+
+  @pytest.mark.django_db
+  def test_new_list_redirects_after_POST(self, client):
+    response = client.post('/lists/add',
+      data={'item_text': 'A new list item'}
+    )
+
+    assert response.status_code == 302
+    assert response['Location'] == testserver_url + '/lists/the-only-list-in-the-world/'
